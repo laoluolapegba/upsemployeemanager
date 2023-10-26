04227de01d2900b3e293619a15dff216acb96681
Olaoluwa Olapegba <laoluolapegba@gmail.com>
2023-10-26 20:25:06 +0100

    add gitignore

---
+++ src/Presentation/UPS.EmployeeManager.UI.WinForms/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UPS.EmployeeManager.Domain.Entities;
+using UPS.EmployeeManager.Shared.Models;
+
+namespace UPS.EmployeeManager.UI.WinForms
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Employee, EmployeeModel>().ReverseMap();            
+         }
+    }
+}
